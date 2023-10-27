f0e38c907007a8ceede7f88ca7544d0dda815584
Pedro <pedro.balestra@outlook.com>
2023-10-27 09:46:32 -0300

    feat: Aplicando o Pattern Command

--- src/br/com/alura/loja/TestesPedidos.java
+++ src/br/com/alura/loja/TestesPedidos.java
@@ -8,13 +8,5 @@ import java.time.LocalDateTime;
 
 public class TestesPedidos {
     public static void main(String[] args) {
-        Orcamento orcamento = new Orcamento(new BigDecimal(600), 4);
-        String cliente = "Ana da Silva";
-        LocalDateTime data = LocalDateTime.now();
-
-        Pedido pedido = new Pedido(cliente, data, orcamento);
-        System.out.println("Salvar pedido no Banco de Dados");
-        System.out.println("Enviar email com dados do novo pedido");
-
     }
 }
