2c82eceea05ff2c22c6f6a0d7f2a3f3a747579f1
Pedro <pedro.balestra@outlook.com>
2023-10-27 11:55:59 -0300

    feat: Aplicando o Pattern Decorator

--- src/br/com/alura/loja/TesteImpostos.java
+++ src/br/com/alura/loja/TesteImpostos.java
@@ -2,6 +2,7 @@ package br.com.alura.loja;
 
 import br.com.alura.loja.imposto.CalculadoraDeImpostos;
 import br.com.alura.loja.imposto.ICMS;
+import br.com.alura.loja.imposto.ISS;
 import br.com.alura.loja.orcamento.Orcamento;
 
 import java.math.BigDecimal;
@@ -12,6 +13,6 @@ public class TesteImpostos {
     public static void main(String[] args) {
         Orcamento orcamento = new Orcamento(new BigDecimal("100"), 1);
         CalculadoraDeImpostos calculadoraDeImpostos = new CalculadoraDeImpostos();
-        System.out.println(calculadoraDeImpostos.calcular(orcamento, new ICMS()));
+        System.out.println(calculadoraDeImpostos.calcular(orcamento, new ICMS(new ISS(null))));
     }
 }
