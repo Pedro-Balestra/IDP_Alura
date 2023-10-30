513d0fcaa6b81296130fca5a6b54b8dde07a457a
Pedro <pedro.balestra@outlook.com>
2023-10-30 11:57:52 -0300

    Atts

--- src/br/com/alura/loja/pedido/acao/EnviarEmailPedido.java
+++
@@ -1,9 +0,0 @@
-package br.com.alura.loja.pedido.acao;
-
-import br.com.alura.loja.pedido.Pedido;
-
-public class EnviarEmailPedido implements AcaoAposGerarpedido {
-    public void executarAcao(Pedido pedido) {
-        System.out.println("Salvar pedido no Banco de Dados");
-    }
-}
