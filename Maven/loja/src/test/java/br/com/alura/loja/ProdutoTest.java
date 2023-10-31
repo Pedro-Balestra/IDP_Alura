8ca1fd78260594adabb8434e6125a1c4f622933d
Pedro <pedro.balestra@outlook.com>
2023-10-31 13:41:34 -0300

    feat: Plugins

--- Maven/loja/src/test/java/br/com/alura/loja/ProdutoTest.java
+++ Maven/loja/src/test/java/br/com/alura/loja/ProdutoTest.java
@@ -1,4 +1,16 @@
 package br.com.alura.loja;
 
+import junit.framework.Assert;
+import org.junit.Test;
+
+import java.math.BigDecimal;
+
 public class ProdutoTest {
+
+    @Test
+    public void test(){
+        Produto p = new Produto("teste", BigDecimal.TEN);
+        Assert.assertEquals("teste", p.getNome());
+        Assert.assertEquals(BigDecimal.TEN, p.getPreco());
+    }
 }
