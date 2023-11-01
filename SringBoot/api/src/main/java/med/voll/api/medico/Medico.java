bf9d29645db390943ea47fc71283480c7424d36a
Pedro <pedro.balestra@outlook.com>
2023-11-01 15:19:45 -0300

    feat: Exclusão lógica

--- SringBoot/api/src/main/java/med/voll/api/medico/Medico.java
+++ SringBoot/api/src/main/java/med/voll/api/medico/Medico.java
@@ -26,7 +26,10 @@ public class Medico {
     @Embedded
     private Endereco endereco;
 
+    private Boolean ativo;
+
     public Medico(DadosCadatroMedico dados) {
+        this.ativo = true;
         this.nome = dados.nome();
         this.email = dados.email();
         this.telefone = dados.telefone();
@@ -46,5 +49,9 @@ public class Medico {
             this.endereco.atulizarInformacoes(dados.endereco());
         }
 
+    }
+
+    public void excluir() {
+        this.ativo = false;
     }
 }
