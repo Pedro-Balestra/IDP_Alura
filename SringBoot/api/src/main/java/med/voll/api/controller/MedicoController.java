abaa82dad8b154f360031d4b4541449dfbb41294
Pedro <pedro.balestra@outlook.com>
2023-11-01 11:16:21 -0300

    feat: Produção de dados na API

--- SringBoot/api/src/main/java/med/voll/api/controller/MedicoController.java
+++ SringBoot/api/src/main/java/med/voll/api/controller/MedicoController.java
@@ -2,11 +2,14 @@ package med.voll.api.controller;
 
 import jakarta.validation.Valid;
 import med.voll.api.medico.DadosCadatroMedico;
+import med.voll.api.medico.DadosListagemMedico;
 import med.voll.api.medico.Medico;
 import med.voll.api.medico.MedicoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/medicos")
@@ -19,6 +22,11 @@ public class MedicoController {
     @Transactional
     public void cadastrar(@RequestBody @Valid DadosCadatroMedico dados){
         repository.save(new Medico(dados));
+    }
+
+    @GetMapping
+    public List<DadosListagemMedico> listar(){
+        return repository.findAll().stream().map(DadosListagemMedico::new).toList();
     }
 
 }
