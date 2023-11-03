2942503804dc4d0707d92502710b577b8609c1c1
Pedro <pedro.balestra@outlook.com>
2023-11-03 11:16:19 -0300

    feat: Autenticando o usuário

--- SpringBoot/api/src/main/java/med/voll/api/infra/security/SecurityFilter.java
+++ SpringBoot/api/src/main/java/med/voll/api/infra/security/SecurityFilter.java
@@ -4,7 +4,10 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import med.voll.api.domain.usuario.UsuarioRespository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
@@ -16,19 +19,30 @@ public class SecurityFilter extends OncePerRequestFilter {
     @Autowired
     private TokenService tokenService;
 
+    @Autowired
+    private UsuarioRespository respository;
+
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
         var tokenJWT = recuperarToken(request);
-        var subject = tokenService.getSubject(tokenJWT);
 
+        if(tokenJWT != null) {
+            var subject = tokenService.getSubject(tokenJWT);
+            var usuario = respository.findByLogin(subject);
+
+            var authentication = new UsernamePasswordAuthenticationToken(usuario,null,usuario.getAuthorities());
+
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+        }
         filterChain.doFilter(request, response);
     }
 
     private String recuperarToken(HttpServletRequest request) {
         var authorizationHeader = request.getHeader("Authorization");
-        if(authorizationHeader == null){
-            throw new RuntimeException("Token JWT não enviado no cabeçalho Authorization");
-        }
-        return authorizationHeader.replace("Bearer", "");
+       if(authorizationHeader != null){
+           return authorizationHeader.replace("Bearer", "");
+
+       }
+       return null;
     }
 }
