5241ee6084264e4988dc408dcb896047b2af7bb8
Pedro <pedro.balestra@outlook.com>
2023-11-06 10:04:55 -0300

    feat: Escolha de médico aleatório

--- SpringBoot/api/src/main/java/med/voll/api/domain/consulta/AgendaDeConsultas.java
+++ SpringBoot/api/src/main/java/med/voll/api/domain/consulta/AgendaDeConsultas.java
@@ -1,6 +1,7 @@
 package med.voll.api.domain.consulta;
 
 import med.voll.api.domain.ValidacaoException;
+import med.voll.api.domain.medico.Medico;
 import med.voll.api.domain.medico.MedicoRepository;
 import med.voll.api.domain.paciente.PacienteRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -35,6 +36,13 @@ public class AgendaDeConsultas {
     }
 
     private Medico escolherMedico(DadosAgendamentoConsulta dados) {
+        if(dados.idMedico() != null){
+            return medicoRepository.getReferenceById(dados.idMedico());
+        }
+        if(dados.especialidade() == null){
+            throw new ValidacaoException("Especialidade é obrigatoria quando o medico não for escolhido!");
+        }
+        return medicoRepository.escolherMedicoAleatorioLivreNaData(dados.especialidade(),dados.data());
     }
 
 }
