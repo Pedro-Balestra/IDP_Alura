ee7b391551cd68db85f0cff79c0e3c4c06cad3a8
Pedro <pedro.balestra@outlook.com>
2023-11-06 16:10:10 -0300

    feat: Testando mais uma funcionalidade

--- Mockito/src/test/java/br/com/alura/leilao/service/FinalizarLeilaoServiceTest.java
+++ Mockito/src/test/java/br/com/alura/leilao/service/FinalizarLeilaoServiceTest.java
@@ -21,11 +21,13 @@ class FinalizarLeilaoServiceTest {
     private FinalizarLeilaoService service;
     @Mock
     private LeilaoDao leilaoDao;
+    @Mock
+    private EnviadorDeEmails enviadorDeEmails;
 
     @BeforeEach
     public void beforeEach(){
         MockitoAnnotations.initMocks(this);
-        this.service = new FinalizarLeilaoService(leilaoDao);
+        this.service = new FinalizarLeilaoService(leilaoDao, enviadorDeEmails);
     }
 
     @Test
@@ -38,6 +40,17 @@ class FinalizarLeilaoServiceTest {
         Assert.assertTrue(leilao.isFechado());
         Assert.assertEquals(new BigDecimal("900"), leilao.getLanceVencedor().getValor());
         Mockito.verify(leilaoDao).salvar(leilao);
+
+    }
+    @Test
+    void deveriaEnviarEmailParaVencedorDoLeilao() {
+        List<Leilao> leilaos = leiloes();
+        Mockito.when(leilaoDao.buscarLeiloesExpirados()).thenReturn(leilaos);
+        service.finalizarLeiloesExpirados();
+
+        Leilao leilao = leilaos.get(0);
+        Lance lanceVencedor = leilao.getLanceVencedor();
+        Mockito.verify(enviadorDeEmails).enviarEmailVencedorLeilao(lanceVencedor);
 
     }
 
