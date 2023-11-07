047c7d6e5988b0a381b06d3fdba1224426f232c6
Pedro <pedro.balestra@outlook.com>
2023-11-07 09:41:23 -0300

    feat: Liberando acesso aos endpoints públicos

--- SpringBoot/forum/src/main/java/br/com/alura/forum/config/validacao/security/SecurityConfiguration.java
+++ SpringBoot/forum/src/main/java/br/com/alura/forum/config/validacao/security/SecurityConfiguration.java
@@ -2,9 +2,35 @@ package br.com.alura.forum.config.validacao.security;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.web.config.EnableSpringDataWebSupport;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @EnableSpringDataWebSupport
 @Configuration
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    //Configurações de Autorização
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+
+        http.authorizeRequests()
+                .antMatchers(HttpMethod.GET,"/topicos").permitAll()
+                .antMatchers(HttpMethod.GET,"/topicos/*").permitAll();
+
+    }
+
+    //Configurações de Autenticação
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+
+    }
+
+    //Configurações de recursos estaticos(js, css, images ...)
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+
+    }
 }
