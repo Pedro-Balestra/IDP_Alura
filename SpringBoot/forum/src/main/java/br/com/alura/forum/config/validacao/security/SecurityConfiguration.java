3c944e402deba92db1ac906123f96aa9fe8873ed
Pedro <pedro.balestra@outlook.com>
2023-11-07 10:04:31 -0300

    feat: Autenticando o usuário

--- SpringBoot/forum/src/main/java/br/com/alura/forum/config/validacao/security/SecurityConfiguration.java
+++ SpringBoot/forum/src/main/java/br/com/alura/forum/config/validacao/security/SecurityConfiguration.java
@@ -1,5 +1,6 @@
 package br.com.alura.forum.config.validacao.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.web.config.EnableSpringDataWebSupport;
 import org.springframework.http.HttpMethod;
@@ -7,10 +8,14 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @EnableSpringDataWebSupport
 @Configuration
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private AutenticacaService autenticacaoService;
 
     //Configurações de Autorização
     @Override
@@ -27,6 +32,8 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     //Configurações de Autenticação
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+
+        auth.userDetailsService(autenticacaoService).passwordEncoder(new BCryptPasswordEncoder());
 
     }
 
