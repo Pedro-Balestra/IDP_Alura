a6474cfd72c0c9d51d7cafb9df7018ac92c491ea
Pedro <pedro.balestra@outlook.com>
2023-11-08 08:53:01 -0300

    feat: Implementando LoginSteps

--- BDD/src/test/java/br/com/alura/leilao/acceptance/steps/LoginSteps.java
+++ BDD/src/test/java/br/com/alura/leilao/acceptance/steps/LoginSteps.java
@@ -29,17 +29,24 @@ public class LoginSteps {
     @Entao("é redirecionado para a pagina de leiloes")
     public void é_redirecionado_para_a_pagina_de_leiloes() {
         Assert.assertTrue(this.leiloesPage.estaNaPaginaDeLeiloes());
+        browser.clean();
     }
 
     @Dado("o usuario invalido")
     public void o_usuario_invalido() {
+        browser = new Browser();
+        browser.seed();
+        loginPage = browser.getLoginPage();
     }
 
     @Quando("tenta se logar")
     public void tenta_se_logar() {
+        this.loginPage.realizaLoginComo("fulano", "xpto");
     }
 
     @Entao("continua na página de login")
     public void continua_na_página_de_login() {
+        Assert.assertTrue(this.loginPage.estaNaPaginaDeLoginComErro());
+        browser.clean();
     }
 }
