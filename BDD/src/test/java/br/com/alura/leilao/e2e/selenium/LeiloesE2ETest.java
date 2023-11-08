5335f884fd4fb96e6a6f786f837fe7b863d8f005
Pedro <pedro.balestra@outlook.com>
2023-11-08 09:25:15 -0300

    feat: Usando Contexto

--- BDD/src/test/java/br/com/alura/leilao/e2e/selenium/LeiloesE2ETest.java
+++ BDD/src/test/java/br/com/alura/leilao/e2e/selenium/LeiloesE2ETest.java
@@ -10,43 +10,45 @@ import br.com.alura.leilao.e2e.pages.LeiloesPage;
 import br.com.alura.leilao.e2e.pages.LoginPage;
 import br.com.alura.leilao.e2e.pages.NovoLeilaoPage;
 
-public class LeiloesE2ETest extends E2ETestBase{
-	
-	private LeiloesPage leiloesPage;
+public class LeiloesE2ETest extends E2ETestBase {
 
-	@BeforeEach
-	void setup() {
-		LoginPage loginPage = new LoginPage(getDriver());
-		leiloesPage = loginPage.realizaLoginComoFulano();		
-	}
+    private LeiloesPage leiloesPage;
 
-	@Test
-	public void deveCadastrarUmLeilao() {
-		NovoLeilaoPage novoLeilaoPage = leiloesPage.visitaPaginaParaCriarUmNovoLeilao();
-		String nome = "Commodore Amiga";
-		String valor = "899.90";
-		String data = "04/08/2020";
-		
-		novoLeilaoPage.preencheForm(nome, valor, data);
-		
-		leiloesPage.esperaCarregar();
+    @BeforeEach
+    void setup() {
+        LoginPage loginPage = new LoginPage(getDriver());
+        leiloesPage = loginPage.realizaLoginComoFulano();
+    }
 
-		assertTrue(leiloesPage.existe(nome, valor, data));
-	}
-	
-	
-	@Test
-	public void deveEditarUmLeilao() {
-		AlterarLeilaoPage novoLeilaoPage = leiloesPage.visitaPaginaParaAltearLeilao();
-		String nome = "Commodore Amiga";
-		String valor = "899.90";
-		String data = "04/08/2020";
-		
-		novoLeilaoPage.preencheForm(nome, valor, data);
-		
-		leiloesPage.esperaCarregar();
+    @Test
+    public void deveCadastrarUmLeilao() {
+        NovoLeilaoPage novoLeilaoPage = leiloesPage.visitaPaginaParaCriarUmNovoLeilao();
+        String nome = "Commodore Amiga";
+        String valor = "899.90";
+        String data = "04/08/2020";
+        String usuario = "fulano";
 
-		assertTrue(leiloesPage.existe(nome, valor, data));
-	}
-	
+        novoLeilaoPage.preencheForm(nome, valor, data);
+
+        leiloesPage.esperaCarregar();
+
+        assertTrue(leiloesPage.existe(nome, valor, data, usuario));
+    }
+
+
+    @Test
+    public void deveEditarUmLeilao() {
+        AlterarLeilaoPage novoLeilaoPage = leiloesPage.visitaPaginaParaAltearLeilao();
+        String nome = "Commodore Amiga";
+        String valor = "899.90";
+        String data = "04/08/2020";
+        String usuario = "fulano";
+
+        novoLeilaoPage.preencheForm(nome, valor, data);
+
+        leiloesPage.esperaCarregar();
+
+        assertTrue(leiloesPage.existe(nome, valor, data, usuario));
+    }
+
 }
