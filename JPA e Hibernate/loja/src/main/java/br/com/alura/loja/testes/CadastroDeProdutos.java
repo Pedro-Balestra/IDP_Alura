ad71d70c371cb46e1f2a675eb3a946ea3636262c
Pedro <pedro.balestra@outlook.com>
2023-11-10 16:09:39 -0300

    feat: Consultando entidades

--- JPA e Hibernate/loja/src/main/java/br/com/alura/loja/testes/CadastroDeProdutos.java
+++ JPA e Hibernate/loja/src/main/java/br/com/alura/loja/testes/CadastroDeProdutos.java
@@ -8,10 +8,23 @@ import br.com.alura.loja.util.JPAUtil;
 
 import javax.persistence.EntityManager;
 import java.math.BigDecimal;
+import java.util.List;
 
 public class CadastroDeProdutos {
 
     public static void main(String[] args) {
+        cadastrarProduto();
+        EntityManager em = JPAUtil.getEntityManager();
+        ProdutoDao produtoDao = new ProdutoDao(em);
+
+        Produto p = produtoDao.buscarPorId(1l);
+        System.out.println(p.getPreco());
+
+        List<Produto> todos = produtoDao.buscarTodos();
+        todos.forEach(p2 -> System.out.println(p.getNome()));
+    }
+
+    private static void cadastrarProduto() {
         Categoria celulares = new Categoria("CELULARES");
         Produto celular = new Produto("Xiaomi Redmi", "Muito legal", new BigDecimal("800"), celulares);
 
